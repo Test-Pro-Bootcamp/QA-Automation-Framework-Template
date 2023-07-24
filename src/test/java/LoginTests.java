32b10d5c767a9d79d9d95cbd48c425a90dfadfa1
Iskuhi <iskuhi.parzyan@testpro.io>
2023-07-24 03:37:43 -0700

    Homework2 6
    Jenkins

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,54 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import Pages.HomePage;
+import Pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import static Pages.BasePage.driver;
+import static Pages.BasePage.url;
+import static org.testng.Assert.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class LoginTests extends BaseTest {
     @Test
-    public void LoginEmptyEmailPasswordTest() {
+    public void renamePlaylist() {
+        String newPlaylistName = "Playlist2023";
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        loginPage.provideEmail("Iparzyan@gmail.com")
+                .providePassword("MkoIkaMarkMia-789")
+                .clickSubmit();
+        homePage.doubleClick();
+        homePage.enterNewPlaylistName(newPlaylistName);
 
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        assertTrue(homePage.playlistExistOrNot(newPlaylistName));
+
     }
+
+    @Test
+    public void loginValidEmailEmptyPassword() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.provideEmail("Iparzyan@gmail.com")
+                .providePassword("")
+                .clickSubmit();
+
+        //Assert.assertEquals(getDriver().getCurrentUrl(),url);
+    }
+
+    @Test
+    public void loginInvalidEmailValidPassword() {
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        loginPage.provideEmail("InvalidEmailAddress")
+                .providePassword("MkoIkaMarkMia-789")
+                .clickSubmit();
+      //  Assert.assertEquals(getDriver().getCurrentUrl(),url);
+
+
+    }
+
 }
