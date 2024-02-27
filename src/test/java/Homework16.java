64890a106cc7732c0edc2396fba32fd4685a706c
Vladislav Sadokha <sadokha.david@gmail.com>
2024-02-27 10:32:30 -0800

    Updated homework16

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -15,11 +15,6 @@ public class Homework16 extends BaseTest {
 
     @Test
     public void registrationNavigation() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        String url = "https://qa.koel.app/";
         driver.get(url);
         WebElement registrationLink = driver.findElement(By.cssSelector("[href='registration']"));
         registrationLink.click();
